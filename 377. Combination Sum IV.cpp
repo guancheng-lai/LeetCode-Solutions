eb17584a9afa9844d4c391ef23fd2ec0d07a2a63
Guancheng Lai <lgc20407@gmail.com>
2020-02-06 19:17:00 -0500

    02/06/2020

--- 377. Combination Sum IV.cpp
+++ 377. Combination Sum IV.cpp
@@ -1,25 +1,36 @@
 class Solution {
 private:    
-	int helper(vector<int> &nums, int currTarget, vector<int> & dp) {
+	unordered_map<int, int> m;
+	
+	int helper(vector<int> &nums, int currTarget) {
 		if (currTarget < 0) return 0;
 		
-		if (dp[currTarget] != -1) return dp[currTarget];
+		if (m.count(currTarget) != 0) return m[currTarget];
 		
 		int result(0);
 		for (int i = 0; i < nums.size(); ++i) {
 			if (currTarget < nums[i]) continue;
 			
-			result += helper(nums, currTarget - nums[i], dp);
+			result += helper(nums, currTarget - nums[i]);
 		}
 		
-		dp[currTarget] = result;
+		m[currTarget] = result;
 		return result;
 	}
 	
 public:
 	int combinationSum4(vector<int>& nums, int target) {
 		if (nums.empty() || target <= 0) return 0;
-		vector<int> dp(target + 1, -1); dp[0] = 1;
-		return helper(nums, target, dp);
+		// vector 
+		// 4 ms     68.85% 
+		// 9.1 MB   30.00%
+		// vector<int> dp(target + 1, -1); dp[0] = 1;
+		// return helper(nums, target, dp);
+		
+		// unordered_map
+		// 4 ms     68.85% 
+		// 10.1 MB   30.00%
+		m[0] = 1;
+		return helper(nums, target);
 	}
 };
