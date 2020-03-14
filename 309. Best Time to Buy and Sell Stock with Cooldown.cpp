5034e3924e229d413a49118ac09c74282ba13ba6
Guancheng Lai <lgc20407@gmail.com>
2020-03-14 18:17:13 -0400

    03/14/2020

--- 309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ 309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if (prices.size() <= 1) return 0;
+        size_t len = prices.size();
+        if (len <= 1) return 0;
         
-        int hold = 0, buy = -prices[0], sell = numeric_limits<int>::min();
-        for (int i = 1, end = prices.size(); i < end; ++i) {
-            buy = max(buy, hold - prices[i]);
-            hold = max(hold, sell);
-            sell = buy + prices[i];
-            
+        vector<int> buy(len), sell(len);
+        buy[0] = -prices[0], sell[0] = 0;
+        buy[1] = max( buy[0],-prices[1] ), sell[1] = max( sell[0],buy[0]+prices[1] );
+        
+        for (int i = 2; i < len; ++i) {
+            buy[i] = max( buy[i-1],sell[i-2]-prices[i] );
+            sell[i] = max( sell[i-1],buy[i-1]+prices[i] );
         }
         
-        return max(hold, sell);
+        return sell.back();
     }
 };
