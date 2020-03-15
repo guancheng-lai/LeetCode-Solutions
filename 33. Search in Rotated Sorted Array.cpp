1553ddc42b9e1d538a51a63ce6c00cee9e0bcea4
Guancheng Lai <lgc20407@gmail.com>
2020-03-14 21:01:18 -0400

    03/14/2020

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <list>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <fstream>
+#include <memory>
+#include <limits>
+#include <numeric>
+using namespace std;
+
+class Solution {
+private:
+	int searchForPivot(vector<int> &nums) {
+		int result;
+		int start = 0, end = nums.size()-1;
+		while (start <= end) {
+			if (start == end) {
+				result = start; 
+				break;
+			}
+			int mid = (start+end+1)/2;
+
+			if (nums[mid] > nums[0]) {
+				start = mid+1;
+			}
+			else {
+				if (nums[mid] < nums[mid-1]) {
+					result = mid;
+					break;	
+				}
+				else {
+					end = mid-1;
+				}               
+			}
+		}
+
+		return result;
+	}
+	
+	int searchForTarget(vector<int> &arr, int start, int end, int target) {
+		int result = -1;
+		while (start < end) {
+			int mid = (start+end+1)/2;
+			if (arr[mid] == target) {
+				result = mid;
+				break;
+			}
+			else if (arr[mid] < target){
+				start = mid+1;
+			} 
+			else {
+				end = mid-1;
+			}
+		}
+		if (start == end && arr[start] == target) result = start;
+		return result;
+	}
+	
+public:
+	int search(vector<int>& nums, int target) {
+		size_t len = nums.size();
+		if (len == 0) {
+			return -1;
+		}
+		if (len == 1) {
+			return (target == nums[0] ? 0 : -1);
+		}
+		
+		int pivotIdx = searchForPivot(nums);
+		if (target == nums[pivotIdx]) {
+			return pivotIdx;
+		}
+		
+		int result = -1;
+		if (target < nums[0]) {
+			result = searchForTarget(nums, pivotIdx, nums.size()-1, target);
+		}
+		else {
+			result = searchForTarget(nums, 0, pivotIdx-1, target);
+		}
+		
+		return result;
+	}
+};
+
+int main(int argc, char *argv[]) {
+	Solution s;
+	int target = 3;
+	vector<int> v{ 3,1 };
+	cout << s.search(v, target);
+	
+//	int pivotIdx = s.searchForPivot(v);
+//	cout << "piovtIdx = " << pivotIdx << ", pivotValue = " << v[pivotIdx] << endl;
+//	if (target < v[0]) {
+//		int found = searchForTarget(v, pivotIdx, v.size()-1, target);
+//		cout << found << " " ;
+//		cout << (found == distance(v.begin(), find(v.begin(), v.end(), target)) ? "CORRECT" : "INCORRECT");
+//	}
+//	else {
+//		int found = searchForTarget(v, 0, pivotIdx, target);
+//		cout << found << " " ;
+//		cout << (found == distance(v.begin(), find(v.begin(), v.end(), target)) ? "CORRECT" : "INCORRECT");
+//	}
+}
