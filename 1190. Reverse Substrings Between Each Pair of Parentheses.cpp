79013f36732376e70479ce07988837fbc90009b7
Guancheng Lai <lgc20407@gmail.com>
2020-09-26 01:22:11 -0400

    09/26/2020

--- 1190. Reverse Substrings Between Each Pair of Parentheses.cpp
+++ 1190. Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -1,27 +1,3 @@
-#include <iostream>
-#include <math.h>
-#include <algorithm>
-#include <string>
-#include <string_view>
-#include <list>
-#include <vector>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <limits>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <future>
-#include <fstream>
-#include <memory>
-#include <limits>
-#include <numeric>
-using namespace std;
-
 class Solution {
 public:
 	string reverseParentheses(string s) {
@@ -30,33 +6,25 @@ public:
 		
 		vector< pair<int,int> > q;
 		stack<int> stk;
-		for (int i = 0; i < N; ++i) 
-		{
-			if (s[i] == '(') stk.push(i);
-			else if (s[i] == ')') 
-			{
+		for (int i = 0; i < N; ++i) {
+			if (s[i] == '(') {
+				stk.push(i);
+			} else if (s[i] == ')') {
 				int left = stk.top(); stk.pop();
 				q.emplace_back( make_pair(left,i) );
 			}
 		}
 		
-		for (auto &[l,r] : q)
-		{
-			std::reverse(s.begin() + l, s.begin() + r);    
+		for (auto&& [l,r] : q) {
+			std::reverse(s.begin() + l, s.begin() + r);
+		}
+
+		for (auto it = s.begin(); it != s.end(); ++it) {
+			if (*it == '(' || *it == ')') {
+				s.erase(it--);
+			}
 		}
 		
-		string res = "";
-		for (auto c : s) 
-		{
-			if (c == '(' || c == ')') continue;
-			
-			res += c;
-		}
-		
-		return res;
+		return s;
 	}
 };
-
-int main(int argc, char *argv[]) {
-	
-}
